777bd177771be0ab89bcdc7a218e2ee2a239e756
vetrivel16 <99579744+vetrivel16@users.noreply.github.com>
2022-02-13 19:52:41 +0530

    factorial.java

---
+++ factorial.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class factorial
+{
+  public static void main (String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the number");
+    int n=sc.nextInt();
+    int result=1;
+    for(int i=1;i<=;i++);
+    {
+      result=result*i;
+    }
+    System.out.println("the factorial of the number is"+result);
+  }
+}
